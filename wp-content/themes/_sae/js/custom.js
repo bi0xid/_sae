ff297aa6d2e02f94022b46e1e7dffbb7ce8dbc27
Rafael Poveda - RaveN <raven@mecus.es>
2013-07-15 11:32:11 +0200

    Unstaging v0.2

--- wp-content/themes/_sae/js/custom.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready( function($) {  
-  $('#top-content .wooslider .wooslider-control-paging').append($('.wooslider-pauseplay a'));
-  
-  $("body img[height='1']").addClass("skip");
-});
